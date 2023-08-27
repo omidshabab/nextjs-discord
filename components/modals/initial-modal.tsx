17cba82cc33b6238be9a9fac228969de07da9cba
Omid Shabab <omidshababdev@gmail.com>
2023-08-27 04:40:55 -0700

    first commit

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import axios from "axios"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 
 import {
      Dialog,
@@ -36,6 +38,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
      const [isMounted, setIsMounted] = useState(false)
+     const router = useRouter()
 
      useEffect(() => {
           setIsMounted(true)
@@ -52,7 +55,15 @@ export const InitialModal = () => {
      const isLoading = form.formState.isSubmitting;
 
      const onSubmit = async (values: z.infer<typeof formSchema>) => {
-          console.log(values)
+          try {
+               await axios.post("/api/servers", values)
+
+               form.reset()
+               router.refresh()
+               window.location.reload()
+          } catch (error) {
+               console.log(error)
+          }
      }
 
      if (!isMounted) {
@@ -80,7 +91,11 @@ export const InitialModal = () => {
                                              render={({ field }) => (
                                                   <FormItem>
                                                        <FormControl>
-                                                            <FileUpload />
+                                                            <FileUpload
+                                                                 endpoint="serverImage"
+                                                                 value={field.value}
+                                                                 onChange={field.onChange}
+                                                            />
                                                        </FormControl>
                                                   </FormItem>
                                              )}
