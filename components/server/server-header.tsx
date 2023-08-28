0d3a92405eb0c402282aa04cd80acab3d9ac28e7
Omid Shabab <omidshababdev@gmail.com>
2023-08-27 20:33:14 -0700

    first commit

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -19,6 +19,7 @@ import {
      DropdownMenuItem,
      DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ServerHeaderProps {
      server: ServerWithMembersWithProfiles
@@ -29,6 +30,8 @@ export const ServerHeader = ({
      server,
      role,
 }: ServerHeaderProps) => {
+     const { onOpen } = useModal()
+
      const isAdmin = role === MemberRole.ADMIN
      const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -48,6 +51,7 @@ export const ServerHeader = ({
                <DropdownMenuContent className="w-56 text-xl font-medium text-black dark:text-neutral-400 space-y-[2px]">
                     {isModerator && (
                          <DropdownMenuItem
+                              onClick={() => onOpen("invite", { server })}
                               className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                          >
                               Invite People
