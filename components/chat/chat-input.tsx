e30808d7c68b6869d43d9656c7b86d21b4d9eea5
Omid Shabab <omidshababdev@gmail.com>
2023-08-30 12:09:59 -0700

    first commit

---
+++ components/chat/chat-input.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+interface ChatInputProps {
+     apiUrl: string
+     query: Record<string, any>
+}
+
+export const ChatInput = () => {
+     return (
+          <div>
+               Chat Input
+          </div>
+     )
+}
