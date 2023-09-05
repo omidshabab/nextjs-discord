cbcd787ddd0bcb9e28d3c8fa9315092f4d038ff8
Omid Shabab <omidshababdev@gmail.com>
2023-09-05 01:00:49 -0700

    first commit

---
+++ components/providers/query-provider.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import {
+     QueryClient,
+     QueryClientProvider
+} from "@tanstack/react-query"
+import { useState } from "react"
+import * as React from 'react';
+
+export const QueryProvider = ({
+     children
+}: { children: React.ReactNode }) => {
+     const [queryClient] = useState(() => new QueryClient())
+
+     return (
+          <QueryClientProvider client={queryClient}>
+               {children}
+          </QueryClientProvider>
+     )
+}
---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,45 @@
+import qs from "query-string";
+import { useParams } from "next/navigation";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/socket-provider";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+export const useClientQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+  const params = useParams();
+
+  const fetchMessages = async ({ pageParam = undefined }) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey],
+      queryFn: fetchMessages,
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+    });
+};
