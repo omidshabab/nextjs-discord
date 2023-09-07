2af6b3f71479aa2f6fbbb2ab66ad7a07856384d2
Omid Shabab <omidshababdev@gmail.com>
2023-09-07 01:49:37 -0700

    first commit

---
+++ hooks/use-chat-socket.ts
@@ -0,0 +1,45 @@
+import { useSocket } from "@/components/providers/socket-provider";
+import { Member, Message, Profile } from "@prisma/client";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+type ChatSocketProps = {
+  addKey: string;
+  updateKey: string;
+  queryKey: string;
+};
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
+
+export const useChatSocket = ({
+  addKey,
+  updateKey,
+  queryKey,
+}: ChatSocketProps) => {
+  const { socket } = useSocket();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+
+    socket.on(updateKey, (message: MessageWithMemberWithProfile) => {
+      queryClient.setQueryData([queryKey], (oldData: any) => {
+        if (!oldData || !oldData.pages || oldData.pages.length === 0) {
+          return oldData;
+        }
+
+        const newData = oldData.pages.map((page: any) => {
+          return {
+            ...page,
+          };
+        });
+      });
+    });
+  }, []);
+};
