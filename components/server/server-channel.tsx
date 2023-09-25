9aa1ec7483333064c8f96103fb93d203ac08c159
Omid Shabab <omidshababdev@gmail.com>
2023-09-25 20:51:48 +0100

    first commit

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -54,7 +54,7 @@ export const ServerChannel = ({
                />
                <p className={cn(
                     "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                    params.channelId == channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                    params?.channelId == channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
                )}>
                     {channel.name}
                </p>
